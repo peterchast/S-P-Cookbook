ece7d77c5ca6f6461fca8fb0eef3e2ead43c6bd9
peterchast <peterchast@gmail.com>
2022-05-19 22:04:01 -1000

    Maja Raja
    Complete overhaul. Do I actually enjoy this site now?!?!

---
+++ static/img/squiggle.js
@@ -0,0 +1,16 @@
+console.log("Hello");
+
+if(!!window.IntersectionObserver){
+	let observer = new IntersectionObserver((entries, observer) => {
+		entries.forEach(entry => {
+		if(entry.isIntersecting){
+			console.log(entry);
+			document.body.classList.add("header-not-at-top");
+			observer.unobserve(entry.target);
+		}
+		});
+	}, {rootMargin: "0px 0px -200px 0px"});
+	document.querySelectorAll('hr').forEach(hr => { observer.observe(hr) });
+};
+
+console.log("Whaaaaat");
